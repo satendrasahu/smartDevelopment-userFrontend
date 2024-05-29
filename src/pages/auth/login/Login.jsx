f36c4d915c40d3a5f0c329e1be9c2d2755229a25
satendra_sahu <ssahu@grepruby.io>
2024-05-29 19:22:12 +0530

    login working fine

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -3,6 +3,10 @@ import { DialogComponent } from "../../../components/dialog";
 import { useDispatch, useSelector } from "react-redux";
 import { handleLoginPopup } from "../../../redux/slices/auth/login.sclice";
 import { useTranslation } from "react-i18next";
+import { StyledLoginContainer } from "./style";
+import { Form, Formik } from "formik";
+import { initialValues, loginSchema } from "./login.schema";
+import { CenteredItemBox, PrimaryText } from "../../../assets/css/common.styles";
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -12,15 +16,32 @@ const Login = () => {
   const closeDialog = () => {
     dispatch(handleLoginPopup(false));
   };
+  const handleSubmitHandler = () =>{}
   return (
     <DialogComponent
-      title={"Register Successfull"}
-      subTitle={"Do you want to login"}
       dialogOpen={toggleLoginPopup}
       handleDialogClose={closeDialog}
-      secondaryButtonTitle={t("cancel")}
-      primaryButtonTitle={t("login")}
-    />
+    >
+      <StyledLoginContainer>
+      <Formik
+          initialValues={initialValues}
+          validationSchema={() => loginSchema(t)}
+          onSubmit={handleSubmitHandler}
+        >
+          {({ values, handleChange, handleBlur }) => (
+            <Form>
+              <CenteredItemBox>
+                <PrimaryText>{t("Login")}</PrimaryText>
+              </CenteredItemBox>
+             {/* <TextField
+             label={t('userNameOrEmail')}
+             icon ={   <AccountCircle />}
+             /> */}
+            </Form>
+          )}
+        </Formik>
+      </StyledLoginContainer>
+      </DialogComponent>
   );
 };
 
--- src/pages/auth/login/style.js
+++ src/pages/auth/login/style.js
@@ -1,3 +1,6 @@
 import { Box, styled } from "@mui/material";
 
-export const StyledLoginContainer = styled(Box)(({ theme, props }) => ({...props}));
+export const StyledLoginContainer = styled(Box)(({ theme, props }) => ({
+  padding: theme.spacing(1),
+  ...props,
+}));
