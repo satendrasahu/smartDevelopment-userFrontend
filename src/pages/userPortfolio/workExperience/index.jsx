5ff26221d1d68f3fadabf9b04cf052cfa0977037
satendra_sahu <ssahu@grepruby.io>
2025-01-03 14:08:25 +0530

    work experience desingend

--- src/pages/userPortfolio/workExperience/index.jsx
+++ src/pages/userPortfolio/workExperience/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Box, useTheme } from "@mui/material";
-import { StyledSkillsContainer } from "./style";
+import {
+  InnerBox,
+  MainBox,
+  ShortNameBox,
+  StyledSkillsContainer,
+} from "./style";
 import { useTranslation } from "react-i18next";
 import {
   CenteredItemBox,
@@ -13,97 +18,103 @@ import Heading1 from "../../../components/ui_kit/Headings/heading1/Heading1";
 export default function WorkExperience() {
   const { t } = useTranslation();
   const theme = useTheme();
-  const educationData = [
+  const experienceData = [
     {
       index: 1,
-      courseName: " Master of Computer Aplication (MCA)",
-      collageName: "Birla Institute of Technology Mesra, Ranchi(Jharkhand).",
+      // desigNationName: "Solution Engineer",
+      // technologyName: "MERN + NEXT",
+      designationTechName:
+        "Solution Engineer | Full Stack Developer (MERN + Next.js)",
+      companyName: "Birla Institute of Technology Mesra, Ranchi(Jharkhand).",
       duration: "2018-2021",
-      collageWebsite: "https://www.bitmesra.ac.in/",
+      companyWebsite: "https://www.bitmesra.ac.in/",
     },
     {
       index: 2,
-      courseName: "BatcherBachelor of Science in Computer Science",
-      collageName:
+      desigNationName: "MERN Stack devloper",
+      technologyName: "MERN + NEXT",
+      designationTechName: "Full Stack Developer (MERN + Next.js)",
+      companyName:
         "Govt. Nirbhay Singh Patel Science College (DAVV), Indore(Madhya Pradesh).",
       duration: "2015-2018",
-      collageWebsite: "https://gnspsc.ac.in/",
+      companyWebsite: "https://gnspsc.ac.in/",
     },
   ];
 
   return (
     <StyledSkillsContainer>
-      <CenteredItemBox>
-        <Heading1
-        //  title={t("workExperience")}
-        />
-      </CenteredItemBox>
-      {educationData?.map((eduData) => {
+      {experienceData?.map((expData) => {
         return (
-          <CenteredItemBox
-            props={{
-              width: "100%",
-              maxWidth: "75%",
-              background: "#fff",
-              width: "100%",
-              padding: "1rem",
-              borderRadius: "0.25rem",
-              cursor: "pointer",
-              transition:
-                "transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out, background-color 0.3s ease-in-out", // Smooth transition for all
-              "&:hover": {
-                transform: "scale(1.05)",
-                boxShadow: "0 10px 30px rgba(0, 0, 0, 0.2)",
-              },
-              [theme.breakpoints.down("md")]: {
-                padding: "0.5rem",
-                maxWidth: "100%",
-              },
-            }}
-            onClick={() => window.open(eduData?.collageWebsite)}
-          >
-            <Box key={uuidv4()}>
+          <MainBox key={uuidv4()}>
+            <ShortNameBox>
               <PrimaryText
                 props={{
-                  color: colors.extra.highLightColor,
                   fontSize: "clamp(1.5rem, 1.75vw, 2rem)",
-                  fontWeight: "clamp(600, 700, 8000)",
-                  fontStyle: "oblique",
-                  fontFamily: "Abril Fatface, serif", //"Cinzel, serif"
-                  transition: "top 0.3s ease, background 0.3s ease",
-                  animation: "fadeIn 2s ease-in-out",
+                  fontWeight: "800",
                   textAlign: "center",
                 }}
               >
-                {eduData?.courseName}
+                {expData?.desigNationName}
               </PrimaryText>
               <PrimaryText
                 props={{
-                  color: colors.extra.darkColor,
                   fontSize: "clamp(1rem, 1.25vw, 1.5rem)",
-                  fontStyle: "italic",
-                  fontFamily: "Spectral, serif",
-                  transition: "top 0.3s ease, background 0.3s ease",
-                  animation: "fadeIn 2.5s ease-in-out",
                   textAlign: "center",
                 }}
+                // onClick={() => window.open(expData?.collageWebsite)}
               >
-                {eduData?.collageName}
+                {expData?.technologyName}
               </PrimaryText>
               <PrimaryText
-                props={{
-                  color: colors.extra.darkColor,
-                  fontSize: "clamp(0.875rem, 1vw, 1rem)",
-                  fontFamily: "Cinzel, serif",
-                  transition: "top 0.3s ease, background 0.3s ease",
-                  animation: "fadeIn 3s ease-in-out",
-                  textAlign: "center",
-                }}
+                props={{ fontSize: "0.875rem", textAlign: "center" }}
               >
-                {eduData?.duration}
+                {expData?.duration}
               </PrimaryText>
-            </Box>
-          </CenteredItemBox>
+            </ShortNameBox>
+            <InnerBox>
+              <Box key={uuidv4()} className="contentBox">
+                <PrimaryText
+                  props={{
+                    color: colors.extra.highLightColor,
+                    fontSize: "clamp(1.5rem, 1.75vw, 2rem)",
+                    fontWeight: "clamp(600, 700, 8000)",
+                    fontStyle: "oblique",
+                    fontFamily: "Abril Fatface, serif", //"Cinzel, serif"
+                    transition: "top 0.3s ease, background 0.3s ease",
+                    animation: "fadeIn 2s ease-in-out",
+                    textAlign: "center",
+                  }}
+                >
+                  {expData?.designationTechName}
+                </PrimaryText>
+                <PrimaryText
+                  props={{
+                    color: colors.extra.darkColor,
+                    fontSize: "clamp(1rem, 1.25vw, 1.5rem)",
+                    fontStyle: "italic",
+                    fontFamily: "Spectral, serif",
+                    transition: "top 0.3s ease, background 0.3s ease",
+                    animation: "fadeIn 2.5s ease-in-out",
+                    textAlign: "center",
+                  }}
+                >
+                  {expData?.companyName}
+                </PrimaryText>
+                <PrimaryText
+                  props={{
+                    color: colors.extra.darkColor,
+                    fontSize: "clamp(0.875rem, 1vw, 1rem)",
+                    fontFamily: "Cinzel, serif",
+                    transition: "top 0.3s ease, background 0.3s ease",
+                    animation: "fadeIn 3s ease-in-out",
+                    textAlign: "center",
+                  }}
+                >
+                  {expData?.duration}
+                </PrimaryText>
+              </Box>
+            </InnerBox>
+          </MainBox>
         );
       })}
     </StyledSkillsContainer>
--- src/pages/userPortfolio/workExperience/style.js
+++ src/pages/userPortfolio/workExperience/style.js
@@ -11,7 +11,6 @@ export const StyledSkillsContainer = styled(Box)(({ theme, props }) => ({
   transition: "background 0.5s ease",
   animation: "fadeIn 1.5s ease-in-out",
   boxShadow: `0 25px 60px rgba(0, 0, 0, 0.15)`,
-  // borderRadius: "0.25rem",
   "@keyframes fadeIn": {
     "0%": {
       opacity: 0,
@@ -26,3 +25,92 @@ export const StyledSkillsContainer = styled(Box)(({ theme, props }) => ({
   },
 }));
 
+export const MainBox = styled(Box)(({ theme }) => ({
+  minHeight: "20rem",
+  maxWidth: "80vw",
+  width: "100%",
+  borderRadius: "10em 0rem 10rem 0rem ",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+
+  gap: "1rem",
+  background: theme.colors.extra.newColor18,
+  [theme.breakpoints.down("xl")]: {
+    maxWidth: "100% !important",
+    width: "100%",
+  },
+
+  [theme.breakpoints.down("md")]: {
+    flexDirection: "column",
+  },
+}));
+
+export const ShortNameBox = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(1),
+  margin: theme.spacing(1),
+  width: "20rem",
+  minWidth: "20rem",
+  minHeight: "20rem",
+  // width:"min-content",
+  borderRadius: "50%",
+  border: `1rem solid ${theme.colors.extra.newColor18}`,
+  opacity: "1",
+  background: theme.colors.extra.newColor18,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  animation: "fadeIn 2s ease-in-out",
+  boxShadow:
+    "inset 5px 5px 5px rgba(0, 0, 0, 0.5), inset -5px -5px 15px rgba(255, 255, 255, 0.5), 5px 5px 15px rgba(0, 0, 0, 0.5), -5px -5px 15px rgba(255, 255, 255, 0.5)",
+  transition:
+    "transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out, background-color 0.3s ease-in-out",
+  "&:hover": {
+    transform: "scale(1.05)",
+  },
+}));
+
+export const InnerBox = styled(Box)(({ theme }) => {
+  return {
+    padding: theme.spacing(1),
+    margin: theme.spacing(1),
+    width: "100%",
+    height: "100%",
+    minHeight: "20rem",
+    opacity: "1",
+    display: "flex",
+    gap: "1rem",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+
+    // [theme.breakpoints.up("lg")]: {
+    borderRadius: "10rem 0.25rem 10rem 0rem ",
+    boxShadow:
+      "inset 5px 5px 5px rgba(0, 0, 0, 0.5), inset -5px -5px 15px rgba(255, 255, 255, 0.5), 5px 5px 15px rgba(0, 0, 0, 0.5), -5px -5px 15px rgba(255, 255, 255, 0.5)",
+    // },
+
+    [theme.breakpoints.down("md")]: {
+      margin: theme.spacing(0),
+    },
+    "& .contentBox": {
+      margin: theme.spacing(3),
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+
+      transition:
+        "transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out, background-color 0.3s ease-in-out",
+      "&:hover": {
+        transform: "scale(1.05)",
+      },
+      [theme.breakpoints.down("lg")]: {
+        margin: theme.spacing(0),
+        borderRadius: "10rem 0.25rem 10rem 0rem ",
+      },
+    },
+  };
+});
